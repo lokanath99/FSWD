d9f981427951ea95583ee9ddf86f80d90b1c5de1
lokanath99 <lokanath1912@gmail.com>
2023-03-02 20:30:02 +0530

    "java script things"

---
+++ JS_demos/demos.js
@@ -0,0 +1,158 @@
+// Variables
+// let fname = null;
+// var lname;
+// const PI = 3.14;
+// fname = "ghh";
+// var lname = 'N';
+// let fname = 'lsodok';
+// console.log(fname);
+// console.log(lname);
+
+// Operators
+// arthematic operators
+// + - * / ** -- ++
+// var a = 3;
+// var b = 2;
+// console.log(++a);//3^2 // a=4
+// console.log(a);
+
+// asignment operator
+// = += -= *= /= **=
+// var a = 6;
+// console.log(a **= 2);//a = a**2
+
+// var bol = true;
+// console.log(typeof(bol));
+// comparision operator
+// var a = '8';
+// var b = 89;
+// console.log(a !== b);//a < b => false !! a == b=> true
+
+// logical operator
+// &&, ||, !
+// var a = 90;
+// var b = 93;
+// var c = 96;
+// //              P   OR  Q
+// console.log( !(a > b) );
+
+// var i = 90;
+// console.log(typeof(i));
+
+// number
+// string
+// var str = 'lokanth"s`';
+// console.log(str.slice(2,7));
+
+// boolean
+// var bool = true;
+// var xyz = null;
+// console.log(xyz);
+
+// object
+//      arrays
+//      objects
+//      date
+
+// var arr = [9, 8, 5, 4, 5, 'sdfasdf'];
+// console.log(arr);
+
+// const car = {
+//     name: "Fiat",
+//     model: "500",
+//     color: "white"
+// };
+
+// const person = {
+//     name: 'lokanath',
+//     age:69,
+//     hight:6.3
+// };
+
+// console.log(person);
+
+// var dt = new Date("02-08-2023");
+// console.log(dt);
+
+// function add(a, b) {
+//     console.log(a);
+//     console.log(b);
+//     return (a+b);
+// }
+
+// var ad = add(8,6);
+// console.log(ad);
+
+// a = 2;
+// if (a === 0)
+//     console.log('whole num');
+// else if( a%2 === 0 ){
+//     console.log('Even');
+// }
+// else{
+//     console.log('Odd');
+// }
+
+// var con = '6';
+// switch (con) {
+//     case 1:
+//         console.log("its a");
+//         break;
+
+//     case 2:
+//         console.log("its b");
+//         break;
+
+//     case 3:
+//         console.log("its c");
+//         break;
+
+//     default:
+//         console.log("its ", con);
+//         break;
+// }
+
+//looping statements
+// while
+// i = 0;
+// do{
+//     console.log('hello');
+//     // statements
+//     i++;
+// }while(i<1)
+
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+// for (let i = 0; i < cars.length; i++) {
+//     console.log(cars[i]);
+// }
+// console.log("`````````````````````````````````");
+// cars.forEach((ele) => {
+//     console.log(ele);
+// });
+// console.log("`````````````````````````````````");
+
+// for (const key in cars) {
+//     if (Object.hasOwnProperty.call(cars, key)) {
+//         const element = cars[key];
+//         console.log(element);
+//     }
+// }
+// console.log("`````````````````````````````````");
+// for (const iterator of cars) {
+//     console.log(iterator);    
+// }
+
+
+// function add(){
+//     return 5;
+// }
+
+// console.log(add())
+
+// var add = (a, b)=>{
+//     return a+b;
+// }
+
+// console.log(add(5,5));
+
