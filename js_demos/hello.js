e2f43ace8421f9d7b4755ee9ab9db22441e5f8dd
“lokanath99” <lokanath1912@gmail.com>
2023-09-07 20:05:40 +0530

    8/9/23

--- js_demos/hello.js
+++ js_demos/hello.js
@@ -8,3 +8,43 @@
 // let a = 93;
 // a = 56;
 // console.log(a);
+
+// operators in java script
+
+// arthematic operators
+
+// a = 10;
+// b = 6;
+// console.log( a  ); //11^2
+
+// assignment operator
+// var a = 9;
+// // a = a-2;
+// a **= 2;//
+// console.log(a);
+
+// comparision operators
+// a = 5;
+// b = 5;
+// console.log(a === b);//return true if the number is eiter less than or equl to
+
+//logical operators
+// and && or || not ! 
+// var a = 20;
+// var b = 3;
+// //             p        q
+// console.log( !(false) );
+
+// var a = 1;
+// // a++;//post incrementation
+// // ++a;//pre incrementation
+
+// console.log( ++a );
+// console.log( a );
+
+var a = 10.556955696366;
+var str = `hello 'lokanath"s' """  ''''' bye `;//'' "" ``
+// var arr = [2,5,6,5,9,5,6];
+// console.log(arr[1]);
+var t_f = false;
+console.log(typeof(t_f));
