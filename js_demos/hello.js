8bd42b1802ba6c54911e37248f67ead9d50394ff
“lokanath99” <lokanath1912@gmail.com>
2023-09-08 20:07:48 +0530

    7/9/23

--- js_demos/hello.js
+++ js_demos/hello.js
@@ -29,7 +29,7 @@
 // console.log(a === b);//return true if the number is eiter less than or equl to
 
 //logical operators
-// and && or || not ! 
+// and && or || not !
 // var a = 20;
 // var b = 3;
 // //             p        q
@@ -42,9 +42,66 @@
 // console.log( ++a );
 // console.log( a );
 
-var a = 10.556955696366;
-var str = `hello 'lokanath"s' """  ''''' bye `;//'' "" ``
+// var a = 10.556955696366;
+// var str = `hello 'lokanath"s' """  ''''' bye `;//'' "" ``
 // var arr = [2,5,6,5,9,5,6];
 // console.log(arr[1]);
-var t_f = false;
-console.log(typeof(t_f));
+// var t_f = false;
+// var a = undefined;
+// console.log(typeof(a));
+
+// Data types
+// Numeric
+// String
+// undifined
+// Null
+// bigint (not needed)
+// symbol (not needed)
+// Object
+//    An Object(JSON)
+//    arrays
+//    date
+
+// Arrays
+// var num = [5, 6, 9, 85, 7, 8, 5, 22, 6, "hi", "hello", [5,6,9,3]];
+// num[5] = 99;
+
+// // num2 = num.concat([5, 6, 9, 8]);
+
+// // num.pop(5)
+// console.log(num[11][1]);
+
+// var car = {
+//     brand: "toyota",//property: val
+//     modelName: "supra",
+//     color: "cyan",
+//     engineCylinders: "v8",
+//     topSpeed: 255,
+//     fuelTankCapacity: 10,
+//     seatingCapacity: [2,4],
+//     engine:{
+//         cylinders: 8,
+//         cc: 6000,
+//         turbo: true
+//     },
+//     drive: function() {
+//         console.log("wroom....");
+//     }
+// }
+
+// console.log(car.engine.turbo);
+
+// Functions
+// function print(){
+//     // statement
+//     console.log('Hello form print...');
+// }
+
+// print()
+
+function add(a, b) {
+    // console.log(a + b);
+    return a + b;
+}
+ret = add(5, 9);
+console.log(ret);
